53c733c892510826354a54d96bffb46f4cb75c6a
guruvirk <gurpindersingh0009@gmail.com>
2022-07-29 12:05:54 +0530

    initial fixes

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Tenant, User } from './services/models';
+import { RoleService } from './services/role.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  currentUser: User;
+  currentTenant: Tenant;
+  isMobile = false;
+  isProgress = false;
+
+  constructor(public auth: RoleService) {
+    this.auth.userChanges.subscribe((user) => {
+      this.currentUser = user;
+    });
+    this.auth.tenantChanges.subscribe((tenant) => {
+      this.currentTenant = tenant;
+    });
+  }
+
+  ngOnInit() {
+    this.currentUser = this.auth.currentUser();
+    this.currentTenant = this.auth.currentTenant();
+  }
+
+  logout() {
+    this.auth.logout()
+  }
+
+}
